44b88d6da351ac629acc3d12edd6a6df791c369b
Mohamad Aid Bacuk Zanab <mohammedsharif067@gmail.com>
2023-09-01 21:13:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+
+// Static method to capitalize the first letter of a string
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  
+  // Static method to sanitize a string
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9-' ]/g, "");
+  }
+
+  // Static method to titleize a string
+  static titleize(str) {
+    const lowercaseWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ');
+
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !lowercaseWords.includes(word.toLowerCase())) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+
+    return titleizedWords.join(' ');
+  }
 }
